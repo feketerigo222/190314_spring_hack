d05cbb5baac462fba23a7d6f3ed81b61680eb067
yamato0331 <yamatotunasima1@gmail.com>
2019-03-13 16:51:43 +0900

    一旦

--- poitto-client/src/components/send/PostIt.js
+++ poitto-client/src/components/send/PostIt.js
@@ -15,7 +15,10 @@ export default class PostIt extends React.Component {
     componentDidMount = () => {
         //console.log(this.curImg);
         const SimpleDrawingBoard = require('simple-drawing-board');
-        this.sdb = new SimpleDrawingBoard(document.getElementById('canvas'));
+        this.sdb = new SimpleDrawingBoard(document.getElementById('canvas'), {
+            lineColor:    '#000',
+            lineSize:     3,
+        });
         this.sdb.ev.on('save', curImg => {
             console.log(this.curImg);
             this.curImg = curImg;
@@ -23,11 +26,18 @@ export default class PostIt extends React.Component {
     }
     
     render() {
+        const colors = ["#FFA5D2", "#FFD9A5", "#FFF5A5", "#C1FFA5", "#A5FFF5", "#A5D5FF", "#CFA5FF"];
+        let colorChange = colors.map(color => {
+            return (
+                <button onClick={this.colorChange.bind(this, color)}>{color}</button>
+            );
+        })
         return (
           <div>
               <canvas id="canvas" width="500" height="500"></canvas>
               <button onClick={this.clearCanvas}>clear</button>
               <button onClick={this.setImg}>send</button>
+              {colorChange}
           </div>
         );
     }
@@ -40,6 +50,11 @@ export default class PostIt extends React.Component {
     setImg = () => {
         this.curImg = this.sdb.getImg();
         this.state.teamRef.push(this.curImg)
+    }
+
+    colorChange = color => {
+        console.log(color)
+        this.sdb.fill(color);
     }
 }
 
