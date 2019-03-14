a0c21670d3e300f4a1d51da0780e2c9efcced0b7
yamato0331 <yamatotunasima1@gmail.com>
2019-03-14 15:56:50 +0900

    ✨ : Add new コンポーネントの製作

--- poitto-client/src/components/postIt/PostIt.js
+++ poitto-client/src/components/postIt/PostIt.js
@@ -3,21 +3,21 @@ import React from 'react';
 export default class PostIt extends React.Component {
     sdb;
     componentDidMount = () => {
-        const SimpleDrawingBoard = require('simple-drawing-board');
-        this.sdb = new SimpleDrawingBoard(document.getElementById(`canvas_${this.props.name}_${this.props.index}`));
-        this.sdb.setImg(this.props.postIt);
+        // const SimpleDrawingBoard = require('simple-drawing-board');
+        // this.sdb = new SimpleDrawingBoard(document.getElementById(`canvas_${this.props.name}_${this.props.index}`));
+        // this.sdb.setImg(this.props.postIt);
     }
 
     componentDidUpdate = () => {
-        this.sdb.setImg(this.props.postIt);
+        //this.sdb.setImg(this.props.postIt);
     }
 
     render() {
         console.log(this.props.postIt)
 
         return (
-            <div className="postIt">
-                <canvas id={`canvas_${this.props.name}_${this.props.index}`} width="80" height="80"></canvas>
+            <div className="postIts">
+                <img src={this.props.postIt} className="postIt"/>
             </div>
         );
     }
