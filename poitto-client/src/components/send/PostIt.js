cb6651c2bfe3bb879a234636f4e5ed8e0d05f7ec
yamato0331 <yamatotunasima1@gmail.com>
2019-03-15 17:18:08 +0900

    ザオリク

--- poitto-client/src/components/send/PostIt.js
+++ poitto-client/src/components/send/PostIt.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { firebaseDB } from '../../firebase';
-
+import penki from '../../images/penki.png';
+import del from '../../images/icon_119860_256.png';
 
 export default class PostIt extends React.Component {
     constructor() {
         super();
-        let teamRef = firebaseDB.ref('team1/topic1/GroupeB');
+        let teamRef = firebaseDB.ref('team1/topic1/GroupeA');
+
         this.state = {
             teamRef
         }
@@ -18,6 +20,7 @@ export default class PostIt extends React.Component {
             lineColor:    '#000',
             lineSize:     3,
         });
+        this.sdb.fill("#FFA5D2");
         this.sdb.ev.on('save', curImg => {
             this.curImg = curImg;
         });
@@ -27,16 +30,20 @@ export default class PostIt extends React.Component {
         const colors = ["#FFA5D2", "#FFD9A5", "#FFF5A5", "#C1FFA5", "#A5FFF5", "#A5D5FF", "#CFA5FF"];
         let colorChange = colors.map(color => {
             return (
-                <button onClick={this.colorChange.bind(this, color)}>{color}</button>
+                <button onClick={this.colorChange.bind(this, color)}></button>
             );
         });
 
         return (
-          <div>
-              <canvas id="canvas" width="500" height="500"></canvas>
-              <button onClick={this.clearCanvas}>clear</button>
-              <button onClick={this.setImg}>send</button>
+          <div className="pictIt">
+              <canvas id="canvas"></canvas>
+              <div className="changeColor">
+              <img src={penki} alt=""></img>
               {colorChange}
+              </div>
+              <button onClick={this.clearCanvas} className="delete">
+              <img src={del} alt=""></img>
+              </button>
           </div>
         );
     }
@@ -54,4 +61,3 @@ export default class PostIt extends React.Component {
         this.sdb.fill(color);
     }
 }
-
