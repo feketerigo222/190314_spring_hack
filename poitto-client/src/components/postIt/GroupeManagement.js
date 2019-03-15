445dd16aba0c4b4efa7e4b5ef2a690b63d6853dc
yamato0331 <yamatotunasima1@gmail.com>
2019-03-15 14:50:42 +0900

    グループの並び替え機能

--- poitto-client/src/components/postIt/GroupeManagement.js
+++ poitto-client/src/components/postIt/GroupeManagement.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Groupe from './Groupe';
 import penki from '../../images/penki.png';
+import Sortable from 'sortablejs';
+import $ from 'jquery';
 
 export default class GroupeManagement extends React.Component {
+  componentDidMount () {
+    Sortable.create($('.groupeManagement')[0], {
+      group: {
+        name: "groupe"
+      },
+      animation: 100
+  });
+  }
+
   render() {
         let groups = [];
         for (let groupe in this.props.currentTopic) {
